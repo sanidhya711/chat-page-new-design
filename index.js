c240e462a23854dffcc61ee8abe3b8450fda7d29
sanidhyajain1 <sanidhyajain077@gmail.com>
2020-12-24 23:06:35 +0530

    swipe functionality works

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let touchstartX = 0;
 let touchendX = 0;
-var message = document.querySelector(".swipe-message");
 
 document.querySelector("body").addEventListener('touchstart', function(event){
     touchstartX = event.changedTouches[0].screenX;
@@ -16,13 +15,11 @@ var users = document.querySelector(".users");
 function handleGesture() {
     if (touchendX+50 <= touchstartX) {
         console.log('Swiped left');
-        message.innerText = "swipe left"
         users.classList.remove("users-swipe-left");
         users.classList.add("users-swipe-right");
     }
     if (touchendX >= touchstartX+50) {
         console.log('Swiped right');
-        message.innerText = "swipe right"
         users.classList.remove("users-swipe-right");
         users.classList.add("users-swipe-left");
     }
